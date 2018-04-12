c78da4b9c8c53242eb2be031700d78f2d202d698
itn3000 <itn3000@gmail.com>
2018-04-12 18:22:11 +0900

    firstcommit

---
+++ DotNet.MsExtConsoleTemplate.CSharp/content/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace content
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ createnupkg.ps1
@@ -0,0 +1,1 @@
+./tools/nuget.exe pack .\DotNet.MsExtConsoleTemplate.CSharp\DotNet.MsExtConsoleTemplate.CSharp.nuspec -OutputDirectory pkg
